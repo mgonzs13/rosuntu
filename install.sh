d0feaa989b64c86ec8cc269519e3231b18bb47d5
Miguel Ángel González Santamarta <mgons@unileon.es>
2024-08-20 00:28:07 +0200

    docker installation fixed

--- install.sh
+++ install.sh
@@ -69,7 +69,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/doc
 chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+apt install docker-ce -y
+groupmod -g 1000 docker
 
 # Plymouth
 echo -e "${YELLOW}========================${RESET}"
